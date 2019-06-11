3e7d4fd01a968c0486b2a7751587c2349162d75f
Taylor Page <jonathantaylorpage@gmail.com>
2019-06-11 12:02:38 -0700

    (feat) Create Nav
    - Modularize nav into it's own component
    - Style Nav
    - Style Header

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
